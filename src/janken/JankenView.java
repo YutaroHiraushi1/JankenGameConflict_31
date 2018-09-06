88f3347db0a81e0a5c92a9c0108f4f5e54e5b81f
Yutaro Hiraushi <t.hiraushi1711@morijyobi.ac.jp>
2018-09-06 17:04:52 +0900

    first commit

---
+++ src/janken/JankenView.java
@@ -0,0 +1,14 @@
+package janken;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JankenView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
